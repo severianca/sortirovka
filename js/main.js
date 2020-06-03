a7c4cd7a711d1d32c8eece5d9022fae27497ca36
maria <malub4@yandex.ru>
2020-06-03 18:49:18 +0500

    Add sort.

--- js/main.js
+++ js/main.js
@@ -15,12 +15,15 @@ function(Component, Page) {
     var button_generate = document.getElementById('button-generate');
     button_generate.addEventListener('click', onButtonGenerateClick);
 
+    var button_visualize = document.getElementById('button-visualize');
+    button_visualize.addEventListener('click', onButtonVisualizeClick);
+
     var sequence_static = document.getElementsByName("number_static");
     var sequence_sort = document.getElementsByName("number_sort");
 
     let sequence = new Array(10);
 
-    function onButtonGenerateClick(event){
+    function onButtonGenerateClick(){
         for (let i=0;i<10;i++){
             sequence[i]= getRandomNumber(0, 100);
             sequence_static[i].innerHTML = sequence[i];
@@ -33,11 +36,34 @@ function(Component, Page) {
         sequence_sort.forEach(number => {
             number.style.color = "blue";
         });
+    }
 
+    function onButtonVisualizeClick(){
+        sort();
+        for (let i=0;i<10;i++){
+            sequence_sort[i].innerHTML = sequence[i];
+        }
     }
 
     function getRandomNumber(min, max){
         return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    /**
+    * сортировка пузырьком
+    */
+    function sort(){
+        let temp;
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 10 - i - 1; j++) {
+                if (sequence[j] > sequence[j + 1]) {
+                    // меняем элементы местами
+                    temp = sequence[j];
+                    sequence[j] = sequence[j + 1];
+                    sequence[j + 1] = temp;
+                }
+            }
+        }
     }
 
 });
