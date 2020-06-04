880057f2ded676055cbb7fd1da05dc446999240d
maria <malub4@yandex.ru>
2020-06-04 20:37:58 +0500

    Added sorting visualization.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 define(['js/Base/Component.js', 'js/Page/Page.js'],
 function(Component, Page) {
 
+    let background_color = "#c0c0c0";
+    let number_color = "blue";
+
     Component.prototype.generateId = function() {
         return Math.random().toString(32).slice(2);
         };
@@ -16,33 +19,36 @@ function(Component, Page) {
     button_generate.addEventListener('click', onButtonGenerateClick);
 
     var button_visualize = document.getElementById('button-visualize');
-    button_visualize.addEventListener('click', onButtonVisualizeClick);
 
     var sequence_static = document.getElementsByName("number_static");
     var sequence_sort = document.getElementsByName("number_sort");
 
     let sequence = new Array(10);
 
+    let index = [];
+    for (let i=0; i<10; i++){
+        index[i] = i;
+    }
+
     function onButtonGenerateClick(){
         for (let i=0;i<10;i++){
-            sequence[i]= getRandomNumber(0, 100);
+            sequence[i]= getRandomNumber(10, 99);
             sequence_static[i].innerHTML = sequence[i];
             sequence_sort[i].innerHTML = sequence[i];
         }
 
         sequence_static.forEach(number => {
-            number.style.color = "blue";
+            number.style.color = number_color;
         });
         sequence_sort.forEach(number => {
-            number.style.color = "blue";
+            number.style.color = number_color;
         });
+
+        button_visualize.addEventListener('click', onButtonVisualizeClick);
     }
 
     function onButtonVisualizeClick(){
-        sort();
-        for (let i=0;i<10;i++){
-            sequence_sort[i].innerHTML = sequence[i];
-        }
+        sort(0,0);
     }
 
     function getRandomNumber(min, max){
@@ -52,18 +58,59 @@ function(Component, Page) {
     /**
     * сортировка пузырьком
     */
-    function sort(){
-        let temp;
-        for (let i = 0; i < 9; i++) {
-            for (let j = 0; j < 10 - i - 1; j++) {
-                if (sequence[j] > sequence[j + 1]) {
-                    // меняем элементы местами
-                    temp = sequence[j];
+    function sort(i,j){
+        let start = Date.now();
+        let timer = setInterval(function() {
+            let timePassed = Date.now() - start;
+          
+            sequence_sort[index[j]].style.color = "black";
+            sequence_sort[index[j+1]].style.color = "black";
+            if (sequence[j] > sequence[j + 1]){
+                let a = window.getComputedStyle(sequence_sort[index[j]]).left;
+                let b = window.getComputedStyle(sequence_sort[index[j+1]]).left;
+                let a2 = Number(a.replace('px',''));
+                let b2 = Number(b.replace('px',''));
+                sequence_sort[index[j]].style.left = a2 + 15 / 50 + 'px';
+                sequence_sort[index[j+1]].style.left = b2 - 15 / 50 + 'px';
+            }
+
+            if (timePassed >= 4240) {
+
+                if (sequence[j] > sequence[j + 1]){
+                    let temp = sequence[j];
                     sequence[j] = sequence[j + 1];
                     sequence[j + 1] = temp;
+                    let temp_index = index[j];
+                    index[j] = index[j+1];
+                    index[j+1] = temp_index;
                 }
-            }
-        }
-    }
 
+                if (i == 9){
+                    sequence_sort[index[j]].style.color = number_color;
+                    sequence_sort[index[j+1]].style.color = number_color;
+                    sequence_sort[index[j]].style.background = background_color;
+                    return;
+                }
+
+                if (j < 8-i){
+                    
+                    sequence_sort[index[j]].style.color = number_color;
+                    sequence_sort[index[j+1]].style.color = number_color;
+                    j++;
+                    clearInterval(timer); // закончить анимацию через n секунды
+                    sort(i,j);
+                }
+                else {
+                    clearInterval(timer); // закончить анимацию через n секунды
+                    sequence_sort[index[j]].style.color = number_color;
+                    sequence_sort[index[j+1]].style.color = number_color;
+                    sequence_sort[index[j+1]].style.background = background_color;
+                    j = 0;
+                    i++;
+                    sort(i,j);
+                }
+
+            }
+        }, 20);
+    }
 });
